ed6aa970eb7cdfcc90e780fe300961e2b006eb59
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-08-31 21:59:15 -0300

    PWA registration added

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 import "./index.css";
 import { MabCalculus } from "./modules/home/view/MabCalculus";
 import { AlertDialogProvider } from "./providers/AlertDialogProvider";
+
+registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
